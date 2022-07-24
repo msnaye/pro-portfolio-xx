ae0530696c14479901c48752c37fd318b7094db1
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-24 15:45:43 -0500

    changed style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode:"class",
+  darkMode: "class",
   content: ["./index.html"],
   theme: {
-    extend: {
-      screens:{
-        "sm":"480px"
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      gray: {
+        100: '#f7fafc',
+        // ...
+        900: '#1a202c',
       },
-      spacing:{
-        "big":"48rem"
+      extend: {
+        screens: {
+          "sm": "480px"
+        },
+        spacing: {
+          "big": "48rem"
+        }
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+      fontFamily: {
+        ibarraRealNova: ['Ibarra-Real-Nova', 'serif']
       }
     },
-    screens:{
-      sm:"480px",
-      md:"768px",
-      lg:"1024px",
-      xl:"1280px",
-    },
-    fontFamily:{
-      nunito:['Nunito', 'sans-serif']
-    }
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
