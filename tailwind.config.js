9363e7c480b1aac9590ce596083853f0181c4959
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-24 02:38:48 -0500

    created cards for projects and for services. Also styled and added fixed image

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
         "big":"48rem"
       }
     },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
     fontFamily:{
       nunito:['Nunito', 'sans-serif']
     }
