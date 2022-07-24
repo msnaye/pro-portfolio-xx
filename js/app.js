26d9c8dd8f34993e8d7161ec03bc56bb81f29477
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-24 03:29:56 -0500

    created footer and working on js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
+const hLink= document.querySelectorAll("#hLink");
 
 hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("hidden");
     hamburger.classList.toggle("bg-white");
+})
+
+hLinks.forEach(link=>{
+    hamburger.addEventListener("click", ()=>{
+        menu.classList.toggle("hidden");
+        hamburger.classList.toggle("bg-white");
+    })
 })
