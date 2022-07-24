ae19666a82d069af13b5739410033a352533979a
msnaye <99111936+msnaye@users.noreply.github.com>
2022-07-23 20:12:31 -0500

    fixed navbar

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-const 
+const hamburger = document.querySelector("#hamburger");
+const menu = document.querySelector("#menu");
+
+hamburger.addEventListener("click", ()=>{
+    menu.classList.toggle("hidden");
+    hamburger.classList.toggle("bg-white");
+})
